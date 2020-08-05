9a3a1685490e733cf7d5eb1e9aa41b93cedbc122
Michele Liese <micheleliesesilva@gmail.com>
2020-08-05 13:24:58 -0300

    adição de logo e estilo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 import styles from './App.module.css';
 import Header from './Components/Header/Header';
+import image from "./Components/images/DesignHome.png";
 
 export default class App extends Component {
     render() {
         return(
-            <div>
-                <Header/>
-                <div className={styles.fundo}></div>
+            <div className = "col-md">
+                <div className = "row-md">
+                    <Header/>
+                </div>
+                <div className = "row-md text-center" style={{backgroundColor: "red"}}>
+                    <div className={styles.fundo}>
+                        <img src={image} className={styles.img}></img>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -24,35 +24,35 @@ export default class Header extends Component {
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md text-center">
+                    <div className="col-md text-center"> 
                         <img src={image} className={styles.logo}></img>
                     </div>
-                    <div className="col-md" style={{alignSelf: "center"}}>
+                    <div className="col-md" style={{alignSelf: "center", paddingTop: "40px"}}>
                         <div className={this.state.selectedPage === 0 ? styles.buttonHeaderSelected : styles.buttonHeaderNotSelected} onClick={()=>{this.onClick(0)}}> 
-                            Inicio 
+                            Home 
                         </div>
                     </div>
-                    <div className="col-md" style={{alignSelf: "center"}}>
+                    <div className="col-md" style={{alignSelf: "center", paddingTop: "40px"}}>
                         <div className={this.state.selectedPage === 1 ? styles.buttonHeaderSelected : styles.buttonHeaderNotSelected}onClick={()=>{this.onClick(1)}}>
                             Projeto
                         </div>
                     </div>
-                    <div className="col-md" style={{alignSelf: "center"}}>
+                    <div className="col-md-2" style={{alignSelf: "center", paddingTop: "40px"}}>
                         <div className={this.state.selectedPage === 2 ? styles.buttonHeaderSelected : styles.buttonHeaderNotSelected}onClick={()=>{this.onClick(2)}}>
                             Representatividade
                         </div>
                     </div>
-                    <div className="col-md" style={{alignSelf: "center"}}>
+                    <div className="col-md" style={{alignSelf: "center", paddingTop: "40px"}}>
                         <div className={this.state.selectedPage === 3 ? styles.buttonHeaderSelected : styles.buttonHeaderNotSelected}onClick={()=>{this.onClick(3)}}>
                             Aulas
                         </div>
                     </div>
-                    <div className="col-md" style={{alignSelf: "center"}}>
+                    <div className="col-md" style={{alignSelf: "center", paddingTop: "40px"}}>
                         <div className={this.state.selectedPage === 4 ? styles.buttonHeaderSelected : styles.buttonHeaderNotSelected}onClick={()=>{this.onClick(4)}}>
                             Podcast
                         </div>
                     </div>
-                    <div className="col-md" style={{alignSelf: "center"}}>
+                    <div className="col-md" style={{alignSelf: "center", paddingTop: "40px"}}>
                         <div className={this.state.selectedPage === 5 ? styles.buttonHeaderSelected : styles.buttonHeaderNotSelected}onClick={()=>{this.onClick(5)}}>
                             Contato
                         </div>
