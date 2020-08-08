8a60840f6427cdb3fe00f37283b3b67afef3bc97
Michele Liese <micheleliesesilva@gmail.com>
2020-08-08 00:35:46 -0300

    encaminhamento para redes sociais

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,8 +5,11 @@ import facebook from "../../Components/images/facebook.png";
 import instagram from "../../Components/images/instagram.png";
 import linkedin from "../../Components/images/linkedin.png";
 import youtube from "../../Components/images/youtube.png";
-
 export default class Home extends Component {
+
+    goToNewTab = (link) => {
+        window.open(link);
+    }
     
     render() {
         return (
@@ -14,21 +17,21 @@ export default class Home extends Component {
                 <div className={styles.background}>
                     <div className="col-sm-12 col-md-12 align-self-center">
                         <div className="row ">
-                            <div className="col-sm-12 col-md-1">
+                            <div className="col-sm-12 col-md-1" style={{paddingTop: "272px"}}>
                                 <div className="row-md-3 d-flex justify-content-center">
-                                    <img src={instagram} className={styles.instagramLogo} alt="instagram"/>
+                                    <img src={instagram} className={styles.instagramLogo} onClick={()=>{this.goToNewTab("https://www.instagram.com/include.gurias/?hl=pt-br")}}></img>
                                 </div>
                                 <div className="row-md-3 d-flex justify-content-center">
-                                    <img src={facebook} className={styles.facebookLogo} alt="facebook"/>
+                                    <img src={facebook} className={styles.facebookLogo} onClick={()=>{this.goToNewTab("https://www.facebook.com/include.GURIAS/?view_public_for=166605734213202")}}></img>
                                 </div>
                                 <div className="row-md-3 d-flex justify-content-center">
-                                    <img src={linkedin} className={styles.linkedinLogo} alt="linkedin"/>
+                                    <img src={linkedin} className={styles.linkedinLogo} onClick={()=>{this.goToNewTab("https://www.linkedin.com/company/includegurias/?viewAsMember=true")}}></img>
                                 </div>
                                 <div className="row-md-3 d-flex justify-content-center">
                                     <img src={youtube} className={styles.youtubeLogo} alt="youtube"/>
                                 </div>
                             </div>
-                            <div className="col-sm-12 col-md-10 ">
+                            <div className="col-sm-12 col-md-10 " style={{paddingTop: "294px"}}>
                                 <div className="row-md-12 d-flex justify-content-center">
                                     <img src={image} className={styles.img} alt="Include Gurias"/>
                                 </div>
