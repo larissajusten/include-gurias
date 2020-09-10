a20c2d06a995fb7520be7d7c0b92e8ba9f5c072c
Guilherme Lague <guilhermelaguebmx@gmail.com>
2020-09-10 17:07:20 -0300

    Ajustes no glossario

--- src/Pages/Projeto/Projeto.jsx
+++ src/Pages/Projeto/Projeto.jsx
@@ -24,40 +24,34 @@ export default class Home extends Component {
                                 </div>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className={"col-md-12 text-center"} style={{background: "rgba(255, 243, 221, 0.54)", height:"680px"}}>
-                                <div className= {styles.titleGlossary + " text-center user-select-none"}>
-                                    GLOSSÁRIO
-                                </div>
-                                <div className= "row" style={{paddingBottom:"50px "}}>
-                                    <div className= "col-md-12 text-center">
-                                        <div className= "row" style={{paddingTop: "50px", paddingLeft: "600px"}}>
-                                            <div className= {styles.subTitleGlossary + " text-center user-select-none"}>
-                                                Include:
-                                            </div>
-                                            <div className= {styles.glossary + " text-left user-select-none"}>
-                                                Termo utilizado na programação para incluir um arquivo em seu projeto. Pode ser uma biblioteca ou um outro arquivo de código fonte.        
-                                            </div>
-                                        </div>
-                                        <div className= "row" style={{paddingTop: "10px", paddingLeft: "600px", paddingRight: "850px"}}>
-                                            <div className= {styles.subTitleGlossary + " text-center user-select-none"}>
-                                                STEM:
-                                            </div>
-                                            <div className= {styles.glossary + " text-left user-select-none"}>
-                                                É um termo em inglês usado para designar o campo do conhecimento compogsto por Ciências, Tecnologia, Engenharia e Matemática (Science, Technology, Engineering and Mathematics).    
-                                            </div>     
-                                        </div>
-                                        <div className= "row" style={{paddingTop: "10px", paddingLeft: "600px"}}>
-                                            <div className= {styles.subTitleGlossary + " text-center user-select-none"}>
-                                                Pensamento Lógico Computacional:
-                                            </div>
-                                            <div className= {styles.glossary + " text-left user-select-none"}>
-                                                Processo de pensamento envolvido na formulação de um problema e na expressão de sua solução.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>    
+                        <div className={"col-md-12"} style={{background: "rgba(255, 243, 221, 0.54)", height:"700px"}}>
+                            <div className= {styles.titleGlossary + " text-center"}>
+                                GLOSSÁRIO
                             </div>
+                            <div className= "row justify-content-start" style={{paddingTop: "50px"}}>
+                                <div className= {styles.subTitleGlossary + " text-right col-md-4"}>
+                                    Include:
+                                </div>
+                                <div className= {styles.glossary + " text-left col-md-6"}>
+                                    Termo utilizado na programação para incluir um arquivo em seu projeto. Pode ser uma biblioteca ou um outro arquivo de código fonte.        
+                                </div>
+                            </div>
+                            <div className= "row justify-content-start" style={{paddingTop: "10px"}}>
+                                <div className= {styles.subTitleGlossary + " text-right col-md-4"}>
+                                    STEM:
+                                </div>
+                                <div className= {styles.glossary + " text-left col-md-6"}>
+                                    É um termo em inglês usado para designar o campo do conhecimento composto por Ciências, Tecnologia, Engenharia e Matemática (Science, Technology, Engineering and Mathematics).    
+                                </div>     
+                            </div>
+                            <div className= "row justify-content-start" style={{paddingTop: "10px"}}>
+                                <div className= {styles.subTitleGlossary + " text-right col-md-6"}>
+                                    Pensamento Lógico Computacional:
+                                </div>
+                                <div className= {styles.glossary + " text-left col-md-4"}>
+                                    Processo de pensamento envolvido na formulação de um problema e na expressão de sua solução.
+                                </div>
+                            </div>  
                         </div>
                         <div className= "row">
                             <div className={"col-md-12 text-center"} style={{background: "white", height: "680px"}}>
